61817884597efad046918ad1651e943c14ff9b3b
David Clements <digidigo@gmail.com>
2022-08-02 13:27:41 -0600

    Updating Readme

--- public/metadata.js
+++ public/metadata.js
@@ -1,7 +1,8 @@
 /**
- * Don't edit
  * 
- * This file will be replaced by the values passed in the form
+ * Author: This will be in the tokenURI data, but does not have a purpose in the NFT Spec
+ * Name: Upon render the token name will be this value + #TOKEN_ID - For example: Seed Phrase #675
+ * Aspect Ratio: This value is used by the launchpad UI and the launchpad backend to render the NFT at the appropriate aspect ratio. 
  */
 
 if (window.nil === undefined) {
